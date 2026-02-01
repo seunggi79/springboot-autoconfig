5e641728bee2cc9b8915e36f9c0a4bfa49e24f63
seunggi <hsg77689469@gmail.com>
2026-02-01 23:59:25 +0900

    @Conditional

--- src/main/java/hello/config/MemoryConfig.java
+++ src/main/java/hello/config/MemoryConfig.java
@@ -1,10 +1,13 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 @Configuration
+@Conditional(MemoryCondition.class) //추가
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
